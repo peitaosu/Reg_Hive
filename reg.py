f087c357629cd1d75fa26453d933ce5117d548ce
Tony Su <peitaosu@163.com>
2018-07-16 22:31:10 +0800

    initial commit - add Registry class

---
+++ reg.py
@@ -0,0 +1,87 @@
+import os, sys, json
+
+class Registry():
+
+    def __init__(self):
+        self.reg = {
+            "HKEY_CLASSES_ROOT": {
+                "Keys": {},
+                "Values": []
+                },
+            "HKEY_CURRENT_USER": {
+                "Keys": {},
+                "Values": []
+                },
+            "HKEY_LOCAL_MACHINE": {
+                "Keys": {},
+                "Values": []
+                },
+            "HKEY_USERS": {
+                "Keys": {},
+                "Values": []
+                },
+            "HKEY_CURRENT_CONFIG": {
+                "Keys": {},
+                "Values": []
+                }
+        }
+    
+    def read_from_reg(self, reg_file_path):
+        # suppose the input file using utf-16 because it's default encoding of regedit exported file
+        try:
+            with open(reg_file_path) as reg_file:    
+                reg_str_list = reg_file.read().decode('utf-16').replace('\\\r\n  ', '').split('\r\n')
+        except:
+            with open(reg_file_path, encoding='utf-16') as reg_file:
+                reg_str_list = reg_file.read().replace('\\\r\n  ', '').split('\r\n')
+        reg_str_list = filter(None, reg_str_list)
+        for reg_str in reg_str_list:
+            if reg_str == 'Windows Registry Editor Version 5.00':
+                continue
+            if reg_str.startswith('['):
+                reg_str = reg_str[1:-1]
+                reg_root = reg_str.split('\\')[0]
+                cur_dict = self.reg[reg_root]
+                for reg_key in reg_str.split('\\')[1:]:
+                    if reg_key not in cur_dict['Keys'].keys():
+                        cur_dict['Keys'][reg_key] = {
+                            "Keys": {},
+                            "Values": []
+                        }
+                        cur_dict = cur_dict['Keys'][reg_key]
+                    else:
+                        cur_dict = cur_dict['Keys'][reg_key]
+                cur_key = cur_dict
+            else:
+                value_name = reg_str.split('=')[0].strip('"')
+                if value_name == '@':
+                    value_name = '(Default)'
+                value_content = '='.join(reg_str.split('=')[1:])
+                if value_content.startswith('"'):
+                    value_type = "REG_SZ"
+                    value_data = value_content.strip('"')
+                elif value_content.startswith('dword'):
+                    value_type = "REG_DWORD"
+                    value_data = value_content.split(':')[1]
+                elif value_content.startswith('qword'):
+                    value_type = "REG_QWORD"
+                    value_data = value_content.split(':')[1]
+                elif value_content.startswith('hex'):
+                    value_type = "REG_BINARY"
+                    value_data = value_content.split(':')[1].replace(',', '')
+                cur_key['Values'].append(
+                    {
+                        "Name": value_name,
+                        "Type": value_type,
+                        "Data": value_data
+                    }
+                )
+    
+    def dump_to_json(self, json_file_path):
+        with open(json_file_path, 'w') as json_file:
+            json.dump(self.reg, json_file, indent=4)
+
+if __name__=="__main__":
+    reg = Registry()
+    reg.read_from_reg(sys.argv[1])
+    reg.dump_to_json(sys.argv[2])
