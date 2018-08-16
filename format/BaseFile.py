ac73a4450013ab2937d98372d702cab363da70ae
Tony Su <peitaosu@163.com>
2018-08-16 22:05:13 +0800

    BaseFile support more operations

--- format/BaseFile.py
+++ format/BaseFile.py
@@ -18,6 +18,38 @@ class BaseFile(object):
         self.obj.seek(self.off + pos)
         self.obj.write(data)
     
+    def read_uint8(self, pos):
+		b = self.read_bin(pos, 1)
+		return unpack('<B', b)[0]
+
+    def write_uint8(self, pos, i):
+		b = pack('<B', i)
+		self.write_bin(pos, b)
+
+    def read_int8(self, pos):
+		b = self.read_bin(pos, 1)
+		return unpack('<b', b)[0]
+
+    def write_int8(self, pos, i):
+		b = pack('<b', i)
+		self.write_bin(pos, b)
+
+    def read_uint16(self, pos):
+		b = self.read_bin(pos, 2)
+		return unpack('<H', b)[0]
+
+    def write_uint16(self, pos, i):
+		b = pack('<H', i)
+		self.write_bin(pos, b)
+
+    def read_int16(self, pos):
+		b = self.read_bin(pos, 2)
+		return unpack('<h', b)[0]
+
+    def write_int16(self, pos, i):
+		b = pack('<h', i)
+		self.write_bin(pos, b)
+
     def read_uint32(self, pos):
         b = self.read_bin(pos, 4)
         return unpack('<L', b)[0]
@@ -40,5 +72,13 @@ class BaseFile(object):
 
     def write_uint64(self, pos, i):
         b = pack('<Q', i)
+        self.write_bin(pos, b)
+    
+    def read_int64(self, pos):
+        b = self.read_bin(pos, 8)
+        return unpack('<q', b)[0]
+
+    def write_int64(self, pos, i):
+        b = pack('<q', i)
         self.write_bin(pos, b)
     
