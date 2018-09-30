39bd21844cda869a09130d2e610399a174c4c85f
Tony Su <peitaosu@163.com>
2018-09-30 23:01:20 +0800

    align single quote and double quote

--- reg.py
+++ reg.py
@@ -6,8 +6,8 @@ class Registry():
         self.reg = {}
         self.reg_str = []
         # suppose the input file using utf-16 because it's default encoding of regedit exported file
-        self.reg_file_encode = 'utf-16'
-        self.regedit_ver = 'Windows Registry Editor Version 5.00'
+        self.reg_file_encode = "utf-16"
+        self.regedit_ver = "Windows Registry Editor Version 5.00"
         self.reg_hive = None
     
     def set_reg_file_encode(self, reg_file_encode):
@@ -25,54 +25,54 @@ class Registry():
     def read_from_reg(self, reg_file_path):
         try:
             with open(reg_file_path) as reg_file:    
-                self.reg_str = reg_file.read().decode(self.reg_file_encode).replace('\\\r\n  ', '').split('\r\n')
+                self.reg_str = reg_file.read().decode(self.reg_file_encode).replace("\\\r\n  ", "").split("\r\n")
         except:
             with open(reg_file_path, encoding=self.reg_file_encode) as reg_file:
-                self.reg_str = reg_file.read().replace('\\\r\n  ', '').split('\r\n')
+                self.reg_str = reg_file.read().replace("\\\r\n  ", "").split("\r\n")
         self.reg_str = filter(None, self.reg_str)
         for reg_str in self.reg_str:
             if reg_str == self.regedit_ver:
                 continue
-            if reg_str.startswith('['):
+            if reg_str.startswith("["):
                 reg_str = reg_str[1:-1]
-                reg_root = reg_str.split('\\')[0]
+                reg_root = reg_str.split("\\")[0]
                 if reg_root not in self.reg:
                     self.reg[reg_root] = {
                         "Keys": {},
                         "Values": []
                         }
                 cur_dict = self.reg[reg_root]
-                for reg_key in reg_str.split('\\')[1:]:
-                    if reg_key not in cur_dict['Keys'].keys():
-                        cur_dict['Keys'][reg_key] = {
+                for reg_key in reg_str.split("\\")[1:]:
+                    if reg_key not in cur_dict["Keys"].keys():
+                        cur_dict["Keys"][reg_key] = {
                             "Keys": {},
                             "Values": []
                         }
-                        cur_dict = cur_dict['Keys'][reg_key]
+                        cur_dict = cur_dict["Keys"][reg_key]
                     else:
-                        cur_dict = cur_dict['Keys'][reg_key]
+                        cur_dict = cur_dict["Keys"][reg_key]
                 cur_key = cur_dict
             else:
-                value_name = reg_str.split('=')[0].strip('"')
+                value_name = reg_str.split("=")[0].strip('"')
                 if value_name == "":
                     value_name = "@"
-                value_content = '='.join(reg_str.split('=')[1:])
+                value_content = "=".join(reg_str.split("=")[1:])
                 if value_content.startswith('"'):
                     value_type = "REG_SZ"
                     value_data = value_content.strip('"')
-                elif value_content.startswith('dword'):
+                elif value_content.startswith("dword"):
                     value_type = "REG_DWORD"
-                    value_data = value_content.split(':')[1]
-                elif value_content.startswith('qword'):
+                    value_data = value_content.split(":")[1]
+                elif value_content.startswith("qword"):
                     value_type = "REG_QWORD"
-                    value_data = value_content.split(':')[1]
-                elif value_content.startswith('hex:'):
+                    value_data = value_content.split(":")[1]
+                elif value_content.startswith("hex:"):
                     value_type = "REG_BINARY"
-                    value_data = value_content.split(':')[1]
-                elif value_content.startswith('hex(2)'):
+                    value_data = value_content.split(":")[1]
+                elif value_content.startswith("hex(2)"):
                     value_type = "REG_EXPAND_SZ"
-                    value_data = value_content.split(':')[1]
-                cur_key['Values'].append(
+                    value_data = value_content.split(":")[1]
+                cur_key["Values"].append(
                     {
                         "Name": value_name,
                         "Type": value_type,
@@ -122,7 +122,7 @@ class Registry():
             break
 
     def dump_to_json(self, json_file_path):
-        with open(json_file_path, 'w') as json_file:
+        with open(json_file_path, "w") as json_file:
             json.dump(self.reg, json_file, indent=4)
 
     def dump_to_reg(self, reg_file_path=None, dump_path=None):
@@ -130,24 +130,24 @@ class Registry():
         self.reg_str.append(self.regedit_ver)
 
         def parse_key(parent_key, parent_str):
-            if len(parent_key['Values']) != 0:
-                self.reg_str.append('\n[{}]'.format(parent_str))
-            for value in parent_key['Values']:
-                if value['Type'] == "REG_SZ":
-                    if value['Name'] == '@':
-                        self.reg_str.append('@="{}"'.format(value['Data']))
+            if len(parent_key["Values"]) != 0:
+                self.reg_str.append("\n[{}]".format(parent_str))
+            for value in parent_key["Values"]:
+                if value["Type"] == "REG_SZ":
+                    if value["Name"] == "@":
+                        self.reg_str.append('@="{}"'.format(value["Data"]))
                     else:
-                        self.reg_str.append('"{}"="{}"'.format(value['Name'], value['Data']))
-                elif value['Type'] == "REG_DWORD":
-                    self.reg_str.append('"{}"=dword:{}'.format(value['Name'], value['Data']))
-                elif value['Type'] == "REG_QWORD":
-                    self.reg_str.append('"{}"=qword:{}'.format(value['Name'], value['Data']))
-                elif value['Type'] == "REG_BINARY":
-                    self.reg_str.append('"{}"=hex:{}'.format(value['Name'], value['Data']))
-                elif value['Type'] == "REG_EXPAND_SZ":
-                    self.reg_str.append('"{}"=hex(2):{}'.format(value['Name'], value['Data']))
-            for key in parent_key['Keys']:
-                parse_key(parent_key['Keys'][key], parent_str + '\\' + key)
+                        self.reg_str.append('"{}"="{}"'.format(value["Name"], value["Data"]))
+                elif value["Type"] == "REG_DWORD":
+                    self.reg_str.append('"{}"=dword:{}'.format(value["Name"], value["Data"]))
+                elif value["Type"] == "REG_QWORD":
+                    self.reg_str.append('"{}"=qword:{}'.format(value["Name"], value["Data"]))
+                elif value["Type"] == "REG_BINARY":
+                    self.reg_str.append('"{}"=hex:{}'.format(value["Name"], value["Data"]))
+                elif value["Type"] == "REG_EXPAND_SZ":
+                    self.reg_str.append('"{}"=hex(2):{}'.format(value["Name"], value["Data"]))
+            for key in parent_key["Keys"]:
+                parse_key(parent_key["Keys"][key], parent_str + "\\" + key)
 
         for root in self.reg:
             key_path = root
@@ -158,9 +158,9 @@ class Registry():
             started = False
             ended = False
             for iter in range(len(self.reg_str[1:])):
-                if self.reg_str[1:][iter].startswith('\n[' + dump_path) and started == False:
+                if self.reg_str[1:][iter].startswith("\n[" + dump_path) and started == False:
                     started = iter
-                if self.reg_str[1:][iter].startswith('\n[') and not self.reg_str[1:][iter].startswith('\n[' + dump_path) and started != False:
+                if self.reg_str[1:][iter].startswith("\n[") and not self.reg_str[1:][iter].startswith("\n[" + dump_path) and started != False:
                     ended = iter
                     break
             if ended == False:
@@ -173,12 +173,12 @@ class Registry():
             matched_reg_str = [self.regedit_ver] + matched_reg_str
         
         if reg_file_path is not None:  
-            with open(reg_file_path, 'w') as reg_file:
+            with open(reg_file_path, "w") as reg_file:
                 if dump_path is None:
-                    reg_file.write('\n'.join(self.reg_str))
+                    reg_file.write("\n".join(self.reg_str))
                     return self.reg_str
                 else:
-                    reg_file.write('\n'.join(matched_reg_str))
+                    reg_file.write("\n".join(matched_reg_str))
                     return matched_reg_str
 
     def dump_to_dat(self, dat_file_path, dump_path, redirect_path = None):
@@ -187,21 +187,21 @@ class Registry():
         if dump_path is None:
             print("[Error] {} CANNOT BE NONE.".format("dump_path"))
             return None
-        reg_root = dump_path.split('\\')[0]
+        reg_root = dump_path.split("\\")[0]
         uuid_str = str(uuid.uuid4())
-        dat_key = reg_root + '\\SOFTWARE\\' + uuid_str
+        dat_key = reg_root + "\\SOFTWARE\\" + uuid_str
         if redirect_path == None:
             redirect_path = dat_key
         else:
-            redirect_path = dat_key + '\\' + redirect_path
+            redirect_path = dat_key + "\\" + redirect_path
         redirected_reg_str = []
         redirected_reg_str.append(self.regedit_ver)
         started = False
         ended = False
         for iter in range(len(self.reg_str[1:])):
-            if self.reg_str[1:][iter].startswith('[' + dump_path) and started == False:
+            if self.reg_str[1:][iter].startswith("[" + dump_path) and started == False:
                 started = iter
-            if self.reg_str[1:][iter].startswith('[') and not self.reg_str[1:][iter].startswith('[' + dump_path) and started != False:
+            if self.reg_str[1:][iter].startswith("[") and not self.reg_str[1:][iter].startswith("[" + dump_path) and started != False:
                 ended = iter
                 break
         if ended == False:
@@ -213,12 +213,12 @@ class Registry():
             return None
         matched_reg_str = [x.replace(dump_path, redirect_path) for x in matched_reg_str]
         redirected_reg_str.extend(matched_reg_str)
-        temp_reg_file = uuid_str + '.reg'
-        with open(temp_reg_file, 'w') as reg_file:
-            reg_file.write('\n'.join(redirected_reg_str))
-        os.system('reg import {}'.format(temp_reg_file))
-        os.system('reg save {} {} /y'.format(dat_key, dat_file_path))
-        os.system('reg delete {} /f'.format(dat_key))
+        temp_reg_file = uuid_str + ".reg"
+        with open(temp_reg_file, "w") as reg_file:
+            reg_file.write("\n".join(redirected_reg_str))
+        os.system("reg import {}".format(temp_reg_file))
+        os.system("reg save {} {} /y".format(dat_key, dat_file_path))
+        os.system("reg delete {} /f".format(dat_key))
         os.remove(temp_reg_file)
         return redirected_reg_str
 
